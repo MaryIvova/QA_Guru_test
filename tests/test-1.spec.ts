a4cc87ee240db827ab47b19968e05ef0d766d551
iMascha <maryzubova@gmail.com>
2025-09-10 23:07:16 +0300

    create articel test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://realworld.qa.guru/#/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('nav').click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('muravjed@list.ru');
+  await page.getByRole('textbox', { name: 'Email' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Asdasd,123');
+  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: ' New Article' }).click();
+  await page.getByRole('textbox', { name: "What's this article about?" }).click();
 });
